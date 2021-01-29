d100659a80239f791439b7250ba27f5a45707968
Sillebob <78139966+Sillebob@users.noreply.github.com>
2021-01-29 21:10:58 +0100

    Add files via upload

---
+++ Metoder og filer opg 1-3 H1/Metoder og filer opg 1-3 H1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Metoder_og_filer_opg_1_3_H1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Opgave 1 Skriv til fil: 
+
+            File.WriteAllText(@".\StarWars.txt", "Han skød først");
+
+            //Opgave 2 læs en tekstfil:
+
+            string content = File.ReadAllText(@".\StarWars.txt");//Udskriver teksten der står i tekstfilen
+            Console.WriteLine(content);
+
+            //Opgave 3 slet en fil:
+
+            File.Delete(@".\StarWats.txt");
+
+            Console.ReadKey();
+
+        }
+    }
+}
